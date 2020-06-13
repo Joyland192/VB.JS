fb495d7c549cf582dba795a0664c80ef51bf38ba
XenocideX7 <cyw4642@gmail.com>
2020-06-13 20:58:12 +0900

    bot upload

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//const { prefix, token } = require('./Config.json'); //prefix, token을 불러옴
-const { prefix } = "++"; //prefix, token을 불러옴
+const { prefix } = require('./Config.json'); //prefix, token을 불러옴
 const token = process.env.token;
 const Discord = require(`discord.js`); // discord.js를 불러옴
 const client = new Discord.Client(); // 새로운 디스코드 클라이언트를 만듬
