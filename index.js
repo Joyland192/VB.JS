78f6d5ad5e37f53ea81156b8ebc7dbcc6d3af009
XenocideX7 <cyw4642@gmail.com>
2020-06-16 15:50:41 +0900

    ㅇ

--- index.js
+++ index.js
@@ -153,20 +153,39 @@ client.on('message', (message) => {
 	message.react('🍊');
 	message.react('🍇');
     }
-    if(message.content.startsWith('!전체공지')) {
-        if(checkPermission(message)) return
-        if(message.member != null) { // 채널에서 공지 쓸 때
-          let contents = message.content.slice('!전체공지'.length);
-          message.member.guild.members.array().forEach(x => {
-            if(x.user.bot) return;
-            x.user.send(`<@${message.author.id}> ${contents}`);
-          });
-          return message.reply('공지를 전송했습니다.');
-        } else {
-          return message.reply('채널에서 실행해주세요.');
-        }
-      }
 });
+client.on('message', (message) => {
+    if(message.author.bot) return;
+  
+    if(message.content == 'ping') {
+      return message.reply('pong');
+    }
+  
+    if(message.content.startsWith('!전체공지')) {
+      if(checkPermission(message)) return
+      if(message.member != null) { // 채널에서 공지 쓸 때
+        let contents = message.content.slice('!전체공지'.length);
+        message.member.guild.members.array().forEach(x => {
+          if(x.user.bot) return;
+          x.user.send(`<@${message.author.id}> ${contents}`);
+        });
+    
+        return message.reply('공지를 전송했습니다.');
+      } else {
+        return message.reply('채널에서 실행해주세요.');
+      }
+    }
+  });
+  
+  function checkPermission(message) {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+      message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
 
 
 
