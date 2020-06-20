c8e396a83fbc1e7d197af52b447ee040a73335d1
XenocideX7 <cyw4642@gmail.com>
2020-06-20 21:35:48 +0900

    ㅇ

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ client.on('message', (message) => {
         message.channel.send(`왜 불러`);
     }
     if (message.content === `${prefix} 프사`) {
-        message.channel.send(message.author.avatarURL);
+        message.channel.send(message.author.avatar);
     }
     if (message.content === `${prefix} 강작가`) {
         message.channel.send(`밥오`);
