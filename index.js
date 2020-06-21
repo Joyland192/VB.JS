c22c3f1bcd2e581315a4d461f8db9a48fedd793b
Joyland192 <48751138+Joyland192@users.noreply.github.com>
2020-06-21 23:22:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ client.on('message', (message) => {
     const args = message.content.split(" ");
     const blueholdcn = client.channels.cache.get('721252326249070652');
     const reinforcejd = client.channels.cache.get('541904100607131658');
-    const namutest = client.channels.cachr.get('697389133361971315');
+    const namutest = client.channels.cache.get('697389133361971315');
     message.author.maxexp = message.author.level*300;
     if (message.content === "0.02" ) {
         // "Pong"으로 되돌려 칩니다.
