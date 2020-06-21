fe3660361f97ee7804e08d6e1a34e0643cd403f7
XenocideX7 <cyw4642@gmail.com>
2020-06-21 21:23:49 +0900

    d

--- index.js
+++ index.js
@@ -79,7 +79,9 @@ client.on('message', (message) => {
     if (message.content === `${prefix} 블홀`) {
         const bluehold = "바보";
     }
-    ((message.content === `${prefix} 블홀은 바보입니까?`) && (bluehold == "바보")) ? message.channel.send('내') : message.channel.send('ㄴ');
+    if (message.content === `${prefix} 블홀은 바보입니까?`) {
+        (bluehold == "바보") ? message.channel.send('내') : message.channel.send('ㄴ');
+    }
     if (message.content === `${prefix} 뭐해`) {
         message.author.what = Math.floor(Math.random() * 5) + 1;
         if (message.author.what === 1) {
