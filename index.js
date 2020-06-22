99532b958149f18e28f503bfb9a34697eb5a9da2
Joyland192 <48751138+Joyland192@users.noreply.github.com>
2020-06-22 20:06:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -212,9 +212,10 @@ client.on('message', (message) => {
     if (message.content === `${prefix} 유튜브`) {
         message.channel.send(joutube);       
     }
-    if ((args[0] === `${prefix}`) && (args[1] === `경험치`) && (args[2] === `조정`)) {
-        message.author.exp = args[3];
-        message.channel.send(`경험치를 ${args[3]}로 조정했습니다.`);
+    if ( message.content.startswith (`${prefix} 경험치 조정`)) {
+        let setexp = message.content.slice (`${prefix} 경험치 조정`.length);
+        message.author.exp = setexp;
+        message.channel.send(`경험치를 ${setexp}로 조정했습니다.`);
     }
    // if (message.content.startswith (`${prefix} 닉네임`)) {
         //let nicknames = message.content.slice(`${prefix} 닉네임`.length);
